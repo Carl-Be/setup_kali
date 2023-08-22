1b308598c13a75b27f7acd491c717d9a1cf9863b
Carl-Be <63538326+Carl-Be@users.noreply.github.com>
2023-08-22 15:17:29 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+if [ "$USER" != "root" ]
+then
+	echo "Please run the script as root user"
+fi
+
+apt update
+
+apt upgrade -y 
+
+apt autoremove
+
+apt clean
+
+apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+
+systemctl enable ssh --now
+
+su kali && python3 -m pip install --upgrade pip
+
+su kali && python3 -m pip install --upgrade pwntools
+
+git clone https://github.com/pwndbg/pwndbg
+
+cd pwndbg && ./setup.sh
+
+apt install ghidra
+
+apt-get install -y kali-desktop-xfce xorg xrdp
+
+sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+
+systemctl enable xrdp --now
